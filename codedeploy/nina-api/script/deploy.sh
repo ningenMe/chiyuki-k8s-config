485308c0022752bbe359e8bf3d12c2559a3b6d60
ningenMe <amohkuru@gmail.com>
2023-02-12 17:19:33 +0900

    [fix]

--- codedeploy/nina-api/script/deploy.sh
+++ codedeploy/nina-api/script/deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+aws ecr get-login-password --region AWS_REGION | docker login --username AWS --password-stdin AWS_ECR_REGISTRY
+docker pull AWS_ECR_REGISTRY/nina-api-repository:latest
+
 cd /home/ec2-user/nina-api
 kubectl apply -f deployment.yaml -n ningenme-space
 kubectl apply -f service.yaml -n ningenme-space
