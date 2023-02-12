bc0be77f6986393c0374da626fce9e036fce0b69
ningenMe <amohkuru@gmail.com>
2023-02-12 20:04:36 +0900

    [fix]

--- codedeploy/env-config/deploy.sh
+++ codedeploy/env-config/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-aws ssm get-parameters-by-path --path "/" --region ap-northeast-1 --output text | awk '{print $5"="$7}' > $HOME/.env
+aws ssm get-parameters-by-path --path "/" --region AWS_REGION --output text | awk '{print $5"="$7}' > $HOME/.env
 kubectl create configmap env-config --from-env-file=$HOME/.env -n ningenme-space -o yaml --dry-run | kubectl apply -f - -n ningenme-space
--- codedeploy/nina-api/deploy.sh
+++ codedeploy/nina-api/deploy.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-aws ecr get-login-password --region AWS_REGION | docker login --username AWS --password-stdin AWS_ECR_REGISTRY
-docker pull AWS_ECR_REGISTRY/nina-api-repository:latest
-docker rmi `docker image ls | grep none | awk '{print $3}'` | true
-
 cd /home/ec2-user/nina-api
 kubectl apply -f deployment.yaml -n ningenme-space
 kubectl apply -f service.yaml -n ningenme-space
