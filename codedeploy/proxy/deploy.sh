6e06092509267bfd719ffd53cd028999e18c3b0b
ningenMe <amohkuru@gmail.com>
2023-04-30 00:18:57 +0900

    [fix]

--- codedeploy/proxy/deploy.sh
+++ codedeploy/proxy/deploy.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-echo hoge
-touch /home/ec2-user/hoge.txt
+lego --accept-tos \
+    --path=/home/ec2-user \
+    --email="amohkuru@gmail.com" \
+    --dns="route53" \
+    --domains="*.ningenme.net" \
+    run
 
-#lego --accept-tos \
-#    --path=/home/ec2-user \
-#    --email="amohkuru@gmail.com" \
-#    --dns="route53" \
-#    --domains="*.ningenme.net" \
-#    run
-#
-#
-#K8S_EC2_DNS=`aws ec2 describe-instances --filters "Name=tag:Name,Values=k8s-ec2" | jq '.Reservations[0]' | jq '.Instances[0]' | jq '.PrivateDnsName' -r`
+touch /home/ec2-user/hoge.txt
+K8S_EC2_DNS=`aws ec2 describe-instances --filters "Name=tag:Name,Values=k8s-ec2" --region ap-northeast-1 | jq '.Reservations[0]' | jq '.Instances[0]' | jq '.PrivateDnsName' -r`
