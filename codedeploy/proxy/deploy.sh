68cb18ba6a842f979357e02d50f9914345654a73
ningenMe <amohkuru@gmail.com>
2023-04-30 00:33:51 +0900

    [fix]

--- codedeploy/proxy/deploy.sh
+++ codedeploy/proxy/deploy.sh
@@ -11,3 +11,7 @@ touch /home/ec2-user/hoge.txt
 K8S_EC2_DNS=`aws ec2 describe-instances --filters "Name=tag:Name,Values=k8s-ec2" --region ap-northeast-1 | jq '.Reservations[0]' | jq '.Instances[0]' | jq '.PrivateDnsName' -r`
 
 sed -i -e s/K8S_EC2_DNS/${K8S_EC2_DNS}/g /home/ec2-user/proxy/server.conf
+
+cp /home/ec2-user/proxy/server.conf /etc/nginx/conf.d/
+
+systemctl restart nginx
