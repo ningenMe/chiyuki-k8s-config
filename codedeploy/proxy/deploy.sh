12c6c20715de5d6ef6c0183a5b60bd40724a4887
ningenMe <amohkuru@gmail.com>
2023-04-30 00:41:57 +0900

    [fix]

--- codedeploy/proxy/deploy.sh
+++ codedeploy/proxy/deploy.sh
@@ -7,7 +7,6 @@ lego --accept-tos \
     --domains="*.ningenme.net" \
     run
 
-touch /home/ec2-user/hoge.txt
 K8S_EC2_DNS=`aws ec2 describe-instances --filters "Name=tag:Name,Values=k8s-ec2" --region ap-northeast-1 | jq '.Reservations[0]' | jq '.Instances[0]' | jq '.PrivateDnsName' -r`
 
 sed -i -e s/K8S_EC2_DNS/${K8S_EC2_DNS}/g /home/ec2-user/proxy/server.conf
